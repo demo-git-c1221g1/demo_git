ede970e01ec2399c8b25d1f286290ce3b7f23fcb
ITlqad1699 <lqad1649engineer@gmail.com>
2022-06-28 15:41:35 +0700

    DaLQA update commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function sum(){
     console.log("aaaaa");
-    console.log("anh đà đẹp trai lắm");
+    console.log("anh đà đẹp trai lắm vip pro siêu cấp");
 }
 function minus(){
     console.log("121212312");
